2627747138f5f27c41715e76c6c3782188b16317
Garima <garimasinghal2285@gmail.com>
2024-04-06 12:06:45 +0530

    B

--- src/p2/Test2.java
+++ src/p2/Test2.java
@@ -3,6 +3,7 @@ package p2;
 public class Test2 {
 	public static void main(String[] args) {
 		System.out.println("In Test 2 file");
-		System.out.println("Line 6 Changes in P2");
+		System.out.println("Line 6 Change in P2");
+		System.out.println("Line 7 Change in P2");
 	}
 }
